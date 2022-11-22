38d6698ee27179274eedfabfa4384cab88e561cf
Daksh <dnakra@bsd220.org>
2022-11-22 16:13:16 -0600

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Daksh Nakra on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
