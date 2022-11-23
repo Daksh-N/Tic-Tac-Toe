dd0f227b52f1157fa21943dfedbbf33939efb120
Daksh <dnakra@bsd220.org>
2022-11-23 14:04:54 -0600

    Created Initial View

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Tic Tac Toe")
+                .fontWeight(.bold)
+                .font(.title)
+            
         }
         .padding()
     }
