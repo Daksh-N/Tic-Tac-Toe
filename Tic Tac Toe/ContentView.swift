9f40caf5d67b7a4fd41c31d52707ec9a4c01d920
Daksh <dnakra@bsd220.org>
2022-11-23 15:05:50 -0600

    Added tap gesture recognizer and changed the color from blue to purple

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count: 9)
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -16,13 +17,16 @@ struct ContentView: View {
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15) {
                 ForEach(0..<9){ index in
                     ZStack {
-                        Color.blue
-                        Text("X")
+                        Color.purple
+                        Text(moves[index])
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
                     }
                     .frame(width: 120, height: 120, alignment: .center)
                     .cornerRadius(30)
+                    .onTapGesture {
+                        moves[index] = "X"
+                    }
                 }
             }
         }
